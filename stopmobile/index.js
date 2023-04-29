8b1cd82fd531c6f54aa636961b2fd11590fb19d5
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2023-04-29 12:02:20 +0300

    start app in ios

--- stopmobile/index.js
+++ stopmobile/index.js
@@ -5,8 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {YaMap} from 'react-native-yamap';
-YaMap.init('e143801e-6824-4b30-bbcf-de724b2d9080');
+// import {YaMap} from 'react-native-yamap';
+// YaMap.init('e143801e-6824-4b30-bbcf-de724b2d9080');
 //YaMap.setLocale('ru_RU'); 
+console.log('appName',appName);
 
 AppRegistry.registerComponent(appName, () => App);
